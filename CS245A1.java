e7eb18efaead5d92ff401f8aef7943f817618a03
denaliwebber <denali.webber@gmail.com>
2019-11-02 16:54:46 -0700

    uploaded more files

--- CS245A1.java
+++ CS245A1.java
@@ -6,6 +6,10 @@ public class CS245A1
 {
 	public static void main(String [] args) throws IOException
 	{
+		String inputFile = args[0];
+		String outputFile = args[1];
+		String type = "trie";
+
 		System.out.println("Reading in configuration file...");
 		Properties prop = new Properties();
 		String fileName = "a1properties.txt";
@@ -15,34 +19,57 @@ public class CS245A1
 		if (is != null)
 		{
 			prop.load(is);
-			String type = prop.getProperty("storage");
+			type = prop.getProperty("storage");
 			if (type.equals("trie"))
+			{
 				System.out.println("Storage type: "+type);
+			}
+
 			else if (type.equals("tree"))
+			{
 				System.out.println("Storage type: "+type);
+			}
+
 			else
+			{
 				System.out.println("Not a valid storage type. Defaulting to trie data structure.");
+			}
 		}
 
 		else
 		{
-			String type = "trie";
 			System.out.println("No configuration file...");
 			System.out.println("Storage type defaults to trie data structure");
 		}
 
-		Scanner cd = new Scanner(System.in);
-		String inputFile = cd.next();
-		System.out.println("Input file name: "+inputFile);
-		Scanner scan = new Scanner(new File("input.txt"));
+		Scanner scan_dict = new Scanner(new File("english.0"));
 		String line;
-		System.out.println("\nContents of input file:");
 
-		while(scan.hasNextLine())
+		PrintStream o = new PrintStream(outputFile);
+
+    	PrintStream console = System.out;
+
+    	System.setOut(o);
+
+		if (type.equals("tree"))
 		{
-    		line = scan.nextLine();
-    		System.out.println(line);
-    	}
+			Tree dictionary = new Tree();
 
-	}
+			while(scan_dict.hasNextLine())
+			{
+    			line = scan_dict.nextLine();
+    			if (!line.equals(""))
+    			{
+    				dictionary.insert(line);
+    				dictionary.print();
+    				System.out.println(line);
+    			}
+    		}
+		}
+    }
 }
+
+//to print to file
+//System.setOut(o);
+//to print to console
+//System.setOut(console);
