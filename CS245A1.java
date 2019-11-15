4123a1c190f1c797d2f1ad7b7c76ebc976a0ddfc
Denali Webber <47125567+denaliwebber@users.noreply.github.com>
2019-11-14 16:28:09 -0800

    Add files via upload

---
+++ CS245A1.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.net.*;
+import java.util.Properties;
+import java.util.Scanner;
+
+public class CS245A1
+{
+	public static void main(String [] args) throws IOException
+	{
+		if (args.length<2)
+			System.out.println("Not enough arguments given on command line.\nPlease run with an input and output file name.");
+		
+		else
+		{
+			String inputFile = args[0];
+			String outputFile = args[1];
+			String type = "trie";
+			Trees dictionary;
+			String line;
+			
+			Properties prop = new Properties();
+			String fileName = "a1properties.txt";
+		
+			InputStream is = CS245A1.class.getClassLoader().getResourceAsStream(fileName);
+
+			if (is != null)
+			{
+				prop.load(is);
+				type = prop.getProperty("storage");
+			}
+
+			if (type.equals("tree"))
+			{
+				dictionary = new Tree();
+			}
+
+			else
+			{
+				dictionary = new Trie();
+			}	
+
+			try
+			{
+    			URL url = new URL("https://raw.githubusercontent.com/magsilva/jazzy/master/resource/dict/english.0");
+        		Scanner scan_dict = new Scanner(url.openStream()); 
+        		while (scan_dict.hasNextLine())
+				{
+    				line = scan_dict.nextLine();
+    				if (!line.equals(""))
+    				{
+    					dictionary.insert(line.toLowerCase());
+    				}
+    			}
+    		}
+
+    		catch(IOException e)
+    		{
+        		e.printStackTrace(); 
+    		}
+
+    		Scanner scan_input = new Scanner(new File(inputFile));
+	    	PrintStream o = new PrintStream(outputFile);
+    		PrintStream console = System.out;
+    		System.setOut(o);
+    	
+    		while(scan_input.hasNextLine())
+    		{
+    			line = scan_input.nextLine();
+    			System.out.println(dictionary.find(line.toLowerCase()));
+    		}
+    	}
+	}
+}
